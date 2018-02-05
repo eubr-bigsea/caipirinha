f82acd64c90fff91c87d73bc5b3ef21c6785be99
Walter dos Santos Filho <waltersf@gmail.com>
2018-02-05 13:32:44 -0200

    Removing old dependencies and upgrade versions.

--- caipirinha/visualization_api.py
+++ caipirinha/visualization_api.py
@@ -3,7 +3,6 @@
 import logging
 import urlparse
 
-import happybase
 from app_auth import requires_auth
 from caipirinha.service import limonero_service
 from flask import current_app, request
@@ -98,57 +97,8 @@ class VisualizationDetailApi(Resource):
                     )
                     result.update(data)
             else:
-                # Legacy code using HBase
-                caipirinha_config = current_app.config['CAIPIRINHA_CONFIG']
-                limonero_config = caipirinha_config['services']['limonero']
-                storage = limonero_service.get_storage_info(
-                    limonero_config['url'],
-                    str(limonero_config['auth_token']),
-                    caipirinha_config['storage_id'])
-
-                # Get HBase hostname and port
-                parsed_url = urlparse.urlparse(storage['url'])
-                host = parsed_url.hostname
-                port = parsed_url.port
-
-                # Create HBase connection
-                connection = happybase.Connection(host=host, port=port)
-                vis_table = connection.table('visualization')
-
-                # Get visualization data and prepare a decoded response
-                # NOTE: Column names in HBase are formatted as
-                # 'col_family:column_name' and
-                # this is the reason for the following key transformation
-
-                row = vis_table.row(
-                    b'{job_id}-{task_id}'.format(job_id=job_id,
-                                                 task_id=task_id))
-                # Close HBase connection
-                connection.close()
-                if row:
-                    data = json.loads(row.get('cf:data'))
-                    labels = [l.strip() for l in
-                              row.get('cf:column_names', '').split(',')]
-                    schema = row.get('cf:schema')
-                    if schema != '':
-                        attributes = json.loads(schema)['fields']
-                    else:
-                        attributes = []
-                else:
-                    data = []
-                    labels = []
-                    attributes = []
-
-                result = dict(
-                    job_id=job_id, task_id=task_id,
-                    suggested_width=visualization.suggested_width,
-                    type=dict(id=visualization.type_id,
-                              icon=visualization.type.icon,
-                              name=visualization.type.name),
-                    title=visualization.title,
-                    labels=labels,
-                    attributes=attributes,
-                    data=data)
+                result = dict(status="ERROR",
+                              message="Missing visualization data"), 400
         # In case of no visualization found
         else:
             result = dict(status="ERROR", message="Not Found"), 404
