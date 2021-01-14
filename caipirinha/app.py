f768cb827649ae4c2896bc550e3ca05a4750f2a5
Walter dos Santos Filho <waltersf@gmail.com>
2021-01-14 12:42:17 -0300

    Adds SQLAlchemy reconnection support

--- caipirinha/app.py
+++ caipirinha/app.py
@@ -25,6 +25,26 @@ from flask_restful import Api, abort
 from caipirinha.visualization_api import VisualizationDetailApi, \
     VisualizationListApi, PublicVisualizationApi
 from caipirinha.text_api import TextListApi, TextDetailApi
+
+from sqlalchemy import exc
+from sqlalchemy import event
+from sqlalchemy.pool import Pool
+
+@event.listens_for(Pool, "checkout")
+def ping_connection(dbapi_connection, connection_record, connection_proxy):
+    cursor = dbapi_connection.cursor()
+    try:
+        cursor.execute("SELECT 1")
+    except:
+        # optional - dispose the whole pool
+        # instead of invalidating one at a time
+        # connection_proxy._pool.dispose()
+
+        # raise DisconnectionError - pool will try
+        # connecting again up to three times before raising.
+        raise exc.DisconnectionError()
+    cursor.close()
+
 sqlalchemy_utils.i18n.get_locale = get_locale
 
 app = Flask(__name__)
